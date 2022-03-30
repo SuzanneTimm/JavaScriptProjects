fd4127c0dc8c2026d73578891b684bd5ee0a5f38
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-03-29 21:27:26 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+
+window.alert("Suzanne loves Hank");                                 //Windows Alert will say Suzanne loves Hank
+
+document.write("I need to eat a pizza!");
+document.write("<br>");
+
+var couple = "Suzanne", man ="Hank";                                //Suzanne will appear in red. Hank will appear in blue
+var coupleColor = couple.fontcolor("red");
+var manColor = man.fontcolor("blue");
+var concatenatedVars = coupleColor + "<br>" + manColor + "<br>";    //Concatenated variables and strings
+
+
+document.write(concatenatedVars);                           
+
+
+document.write("I got a feeling, "                                   //Concatenated strings
++ "that tonight's going to be a good, good night!" 
++ "<br>"
++ "Black Eyed Peas"
++ "<br>");
+
+
+document.write(10+10);                                              //an expression
+
