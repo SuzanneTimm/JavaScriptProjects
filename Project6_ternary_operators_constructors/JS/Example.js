8953742a8edebf9efcff3cf89a8377372904e478
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-03-31 17:42:04 -0700

    Project 6

---
+++ Project6_ternary_operators_constructors/JS/Example.js
@@ -0,0 +1,12 @@
+//nested function
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+      var Starting_point = 1;
+      function Plus_one() {
+        Starting_point += 1;
+      }
+      Plus_one();
+      return Starting_point;
+    }
+  }
